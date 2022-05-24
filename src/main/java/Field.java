1fc76e17e7825b05a6eb69e33f83ed4e16553746
Peszko-Patryk <patrickpeszko@gmail.com>
2022-05-24 14:54:45 +0200

    Repaired window size and location. Added default values for number of columns and rows. Added validation of dimensions passed as arguments.

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -25,6 +25,7 @@ public class Field extends JPanel {
                 gameField[i][j] = random.nextInt(2);
             }
         }
+        super.setPreferredSize(new Dimension(colNumber * cellSize, rowNumber * cellSize));
     }
 
     @Override
--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -4,44 +4,51 @@ import java.awt.*;
 public class GameOfLife {
 
 
-    private static int rowNumber;
-    private static int colNumber;
+    private static final int DEFAULT_CELL_NUMBER = 5;
+    private static int rowNumber = DEFAULT_CELL_NUMBER;
+    private static int colNumber = DEFAULT_CELL_NUMBER;
 
 
     public static void main(String[] args) {
-        if (!checkArgs(args)) {
-            return;
-        }
+        checkArgs(args);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int cellSize = (int) screenSize.getHeight() / rowNumber;
+        int cellSize = Math.min((int) ((screenSize.getHeight() - 100) / rowNumber),
+                (int) ((screenSize.getWidth() - 100) / colNumber));
 
         Field field = new Field(rowNumber, colNumber, cellSize);
         JFrame frame = new JFrame();
 
-        frame.setSize(colNumber * cellSize + 20, rowNumber * cellSize + 20);
         frame.setLocationRelativeTo(null);
         frame.setTitle("Game Of Life");
         frame.setResizable(true);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(field);
+        frame.pack();
+        int panelX = (int) ((screenSize.getWidth() - frame.getWidth() - frame.getInsets().left - frame.getInsets().right) / 2);
+        int panelY = (int) ((screenSize.getHeight() - frame.getHeight() - frame.getInsets().top - frame.getInsets().bottom) / 2);
+        frame.setLocation(panelX, panelY);
     }
 
-    private static boolean checkArgs(String[] args) {
+    private static void checkArgs(String[] args) {
         for (int i = 0; i < args.length; i++) {
             switch (args[i].toUpperCase()) {
                 case "-R":
-                    rowNumber = Integer.parseInt(args[++i]);
+                    int rNumber = Integer.parseInt(args[++i]);
+                    if (rNumber > 0 && rNumber <= 100) {
+                        rowNumber = rNumber;
+                    }
                     break;
                 case "-C":
-                    colNumber = Integer.parseInt(args[++i]);
+                    int cNumber = Integer.parseInt(args[++i]);
+                    if (cNumber > 0 && cNumber <= 100) {
+                        colNumber = cNumber;
+                    }
                     break;
                 default:
                     System.out.println("Wrong parameters. Add -C flag to specify number of columns. Add -R flag to specify number of rows. ");
-                    return false;
             }
         }
-        return true;
     }
 }
